f98c34787994f3588cf375da4c72da82fae2f8b6
Rodrigo Feliciano <pakequis@gmail.com>
2015-11-19 19:45:29 -0200

    Ajustes nos textos e identação

--- contador_truco.ino
+++ contador_truco.ino
@@ -3,7 +3,6 @@
         
         Rodrigo Feliciano (www.pakequis.com.br)
                    Novembro de 2015
-
 ATENCAO: Utiliza o shield de LCD 2x16 com teclado!!!
 ********************************************************/
 #include <LiquidCrystal.h>
@@ -22,9 +21,9 @@ int adc_key_in  = 0;
 #define btnNONE   5
 
 //variaveis...
-int dupla1 = 0; 
-int dupla2 = 0;
-int l_select = 0;
+int dupla1 = 0; 	//Pontos da dupla 1
+int dupla2 = 0;		//Pontos da dupla 2
+int l_select = 0;	//selecao de linha
 
 // read the buttons
 int read_LCD_buttons()
@@ -39,12 +38,12 @@ int read_LCD_buttons()
 	//if (adc_key_in < 650)  return btnLEFT; 
 	//if (adc_key_in < 850)  return btnSELECT;  
 
-        //Meu Shield eh versao 1.0
-        if (adc_key_in < 50)   return btnRIGHT;  
-        if (adc_key_in < 195)  return btnUP; 
-        if (adc_key_in < 380)  return btnDOWN; 
-        if (adc_key_in < 555)  return btnLEFT; 
-        if (adc_key_in < 790)  return btnSELECT;   
+	//Meu Shield eh versao 1.0
+	if (adc_key_in < 50)   return btnRIGHT;  
+	if (adc_key_in < 195)  return btnUP; 
+	if (adc_key_in < 380)  return btnDOWN; 
+	if (adc_key_in < 555)  return btnLEFT; 
+	if (adc_key_in < 790)  return btnSELECT;   
 
 	return btnNONE;  // when all others fail, return this...
 }
@@ -57,7 +56,8 @@ void setup()
  
 void loop()
 {
-        delay(100);
+	delay(100);
+
 	//Tela do contador
 	lcd.setCursor(0,0);	      
 	lcd.print("  Dupla 1 = ");
@@ -65,11 +65,11 @@ void loop()
 	lcd.print("  Dupla 2 = ");
  
 	lcd.setCursor(12,0);
-	lcd.print(dupla1);
-        lcd.print(" ");
+	lcd.print(dupla1);	//Mostra pontos da dupla 1
+	lcd.print(" ");
 	lcd.setCursor(12,1);
-	lcd.print(dupla2);
-        lcd.print(" ");
+	lcd.print(dupla2);	//Mostra pontos da dupla 2
+	lcd.print(" ");
         
 	//Qual contador estah selecionado
 	if(!l_select)
@@ -95,7 +95,7 @@ void loop()
 	}
 
 	//Condicao 12/0: Regra do truco de churraso 
-        //onde o perdedor tem que passar debaixo da mesa
+	//onde o perdedor tem que passar debaixo da mesa
 	if((dupla1 == 12) && (dupla2 == 0))
 	{
 		lcd.setCursor(0,0);
@@ -111,11 +111,11 @@ void loop()
 		}
 	}
 	//Condicao 12/0: Regra do truco de churraso 
-        //onde o perdedor tem que passar debaixo da mesa
+	//onde o perdedor tem que passar debaixo da mesa
 	if ((dupla1 == 0) && (dupla2 == 12))
 	{
 		lcd.setCursor(0,0);
-                lcd.print("Dupla 1: Passa  ");
+		lcd.print("Dupla 1: Passa  ");
 		lcd.setCursor(0,1);
 		lcd.print("debaixo da mesa!");
 		dupla1 = 0;
@@ -127,7 +127,7 @@ void loop()
 		}
 	} 
 	
-	//Um dos contadores chegou a 12
+	//Dupla 1 chegou a 12
 	if ((dupla1 == 12) && (dupla2 > 0))
 	{
 		dupla1 = 0;
@@ -143,7 +143,7 @@ void loop()
 		}
 	}
 
-	//Um dos contadores chegou a 12
+	//Dupla 2 chegou a 12
 	if ((dupla1 > 0) && (dupla2 == 12))
 	{
 		dupla1 = 0;
@@ -162,61 +162,62 @@ void loop()
 	lcd_key = read_LCD_buttons();
 	
 	//Atualizacao de variaveis quando uma
-        //tecla for pressionada
+	//tecla for pressionada
 	switch (lcd_key)          
 	{
 		case btnRIGHT:
 		{
 			if(!l_select)
 			{
-				if(dupla1 < 12) dupla1++;
+				if(dupla1 < 12) dupla1++;	//soma 1 ponto para a dupla 1
 			}else
 			{
-				if(dupla2 < 12) dupla2++;
+				if(dupla2 < 12) dupla2++;	//soma 1 ponto para a dupla 2
 			}
-                        delay(100);
+			delay(100);
 			break;
 		}
    		case btnLEFT:
    			{
    				if(!l_select)
    				{
-   					if(dupla1) dupla1--;
+   					if(dupla1) dupla1--;	//diminui 1 ponto para a dupla 1
    				}else
    				{
-   					if(dupla2) dupla2--;
+   					if(dupla2) dupla2--;	//diminui 1 ponto para a dupla 2
    				}
-                                delay(100);
+   				delay(100);
    				break;
    			}
    		case btnUP:
    		{
-   			if(l_select) l_select--;
-                        delay(100);
+   			if(l_select) l_select--;	//vai para a primeira linha
+   			delay(100);
    			break;
    		}
    		case btnDOWN:
    		{
-   			if(!l_select) l_select++;
-                        delay(100);
+   			if(!l_select) l_select++;	//vai para a segunda linha
+   			delay(100);
    			break;
    		}
+		//zera os contadores e limpa o display
    		case btnSELECT:
    		{
    			dupla1 = 0;
    			dupla2 = 0;
    			l_select = 0;
-                        delay(100);
-                        lcd.setCursor(0,0);
-                        lcd.print("                ");
-                        lcd.setCursor(0,1);
-                        lcd.print("                ");
+   			delay(100);
+   			lcd.setCursor(0,0);
+   			lcd.print("                ");
+   			lcd.setCursor(0,1);
+   			lcd.print("                ");
    			break;
    		}
-    	case btnNONE:
-    	{
-    		//vazio!!!
-    		break;
+    		case btnNONE:
+    		{
+    			//vazio!!!
+    			break;
+    		}
     	}
-	}
 }
